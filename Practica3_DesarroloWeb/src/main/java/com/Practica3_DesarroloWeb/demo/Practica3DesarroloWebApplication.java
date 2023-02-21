4bf40da4c2365affe72b940c37611fa718f1ed42
marianaalvarezvillalta <marianaalv07@gmail.com>
2023-02-20 20:29:09 -0600

    creación del proyecto

---
+++ Practica3_DesarroloWeb/src/main/java/com/Practica3_DesarroloWeb/demo/Practica3DesarroloWebApplication.java
@@ -0,0 +1,13 @@
+package com.Practica3_DesarroloWeb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica3DesarroloWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica3DesarroloWebApplication.class, args);
+	}
+
+}
---
+++ Practica3_DesarroloWeb/src/test/java/com/Practica3_DesarroloWeb/demo/Practica3DesarroloWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Practica3_DesarroloWeb.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica3DesarroloWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
